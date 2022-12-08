85e90dd6a9031600bfe98818e66bc2b88ce6a4f5
nelson6868 <ekwomnelson090@gmail.com>
2022-12-08 22:21:24 +0100

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Nelson", 1000);
+            Console.Title = "ATM Console App";
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+        }
+    }
+}
